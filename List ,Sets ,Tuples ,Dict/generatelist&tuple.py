e8987b2368202fb90799188a5d910e713042d059
sabam <sabasam31@gmail.com>
2023-07-25 20:03:48 +0530

    Generating List & tuple from a sequence of numbers

---
+++ List ,Sets ,Tuples ,Dict/generatelist&tuple.py
@@ -0,0 +1,20 @@
+'''
+@Author: Saba Muttagi
+@Date: 2023-07-25
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-07-25
+@Title : Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.
+Sample data : 3, 5, 7, 23
+Output :
+List : ['3', ' 5', ' 7', ' 23']
+Tuple : ('3', ' 5', ' 7', ' 23')
+'''
+
+X=input("Enter your data: ")
+
+mylist=X.split(",")
+List1=list((X))
+print("List :",mylist)
+
+tuple2=X.split(",")
+print("Tuple :",tuple(tuple2))
