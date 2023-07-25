17245486930115fec779c65b0508cf6cef7ab3b7
sabam <sabasam31@gmail.com>
2023-07-25 18:59:15 +0530

    Lowering first n character of string

---
+++ Basic python/lowercase1stchar.py
@@ -0,0 +1,15 @@
+
+'''
+@Author: Saba Muttagi
+@Date: 2023-07-23
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-07-23
+@Title : Python program for lowering the first n character of a string
+'''
+
+first="LOWERING the first characters"
+print("The original string is: "+str(first))
+
+result=first[:5].lower()
+
+print("The string after lowering first nth character :" +str(result))
