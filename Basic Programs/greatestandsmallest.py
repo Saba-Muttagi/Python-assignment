6fd8bc9293eac4c866d16a35546d3af1045d18a6
sabam <sabasam31@gmail.com>
2023-07-25 18:39:13 +0530

    Finding Greatest & Smallest of number

---
+++ Basic Programs/greatestandsmallest.py
@@ -0,0 +1,17 @@
+'''
+@Author: Saba Muttagi
+@Date: 2023-07-24
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-07-24
+@Title : Python program for Greatest of 3 numbers and smallest of 3 numbers.
+'''
+
+#Greatest of three
+A=max(30,167,89)
+print(A)
+
+
+#Smallest of three
+B=min(30,167,89)
+print(B)
+  
