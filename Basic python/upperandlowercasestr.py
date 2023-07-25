0ba7cd425ac8740d21e6a55339d7b3d9a547f630
sabam <sabasam31@gmail.com>
2023-07-25 19:01:52 +0530

    Converting string into Upper and lowercase

---
+++ Basic python/upperandlowercasestr.py
@@ -0,0 +1,16 @@
+'''
+@Author: Saba Muttagi
+@Date: 2023-07-23
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-07-23
+@Title : Python program for lowercase and uppercase of a string
+'''
+
+inp = input("Enter user input:")
+print("User input is: " + inp)
+
+x=inp.upper()
+y=inp.casefold()
+
+print(x)
+print(y)
