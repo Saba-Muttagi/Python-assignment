204ed09fc08240ed4fe9685161af136d051f3a0a
sabam <sabasam31@gmail.com>
2023-07-25 18:41:53 +0530

    Checking given letter is Vowel or not

---
+++ Basic Programs/Vowelornot.py
@@ -0,0 +1,20 @@
+'''
+@Author: Saba Muttagi
+@Date: 2023-07-22
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-07-24
+@Title : Python program to to check if the given character is Vowel or Not
+'''
+
+x=input("Enter the character:")
+vowels=["a","e","i","o","u","A","E","I","O","U"]
+#for x in vowels:
+if x in vowels:
+ print("Given character is Vowel")  
+else:
+ print("It's Consonent")
+   
+   
+
+
+
