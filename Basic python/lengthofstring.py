1c1ee39a63923aebffa81da15d640af3de4fc29d
sabam <sabasam31@gmail.com>
2023-07-25 18:55:32 +0530

    Finding the length of a string

---
+++ Basic python/lengthofstring.py
@@ -0,0 +1,14 @@
+
+'''
+@Author: Saba Muttagi
+@Date: 2023-07-23
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-07-23
+@Title : Python program to find the length of a string
+'''
+x="Python is an interpreted language"
+print("Length of string is:",len(x))
+
+y="The Temperature reached 36 degrees Celsius"
+print("Length of string is:")
+print(len(y))
