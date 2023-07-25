87c9097d6d21cad4877110c06ee6ad3f86ca07a2
sabam <sabasam31@gmail.com>
2023-07-25 19:00:42 +0530

    Reversing the given string

---
+++ Basic python/reverseofstring.py
@@ -0,0 +1,9 @@
+'''
+@Author: Saba Muttagi
+@Date: 2023-07-23
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-07-23
+@Title : Python program for reversing a string
+'''
+x="Reversing a given string"[::-1]
+print(x)
