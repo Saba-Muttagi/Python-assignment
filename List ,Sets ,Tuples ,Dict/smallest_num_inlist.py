a35410d34d8a3a00233cf651bf2a7be1a86fe989
sabam <sabasam31@gmail.com>
2023-07-27 22:52:47 +0530

    To get the smallest number from a list

---
+++ List ,Sets ,Tuples ,Dict/smallest_num_inlist.py
@@ -0,0 +1,12 @@
+
+'''
+@Author: Saba Muttagi
+@Date: 2023-07-27
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-07-27
+@Title : Python program to get the smallest number from a list.
+'''
+
+List_a=[12,5,13,6,8,10,45]
+V=min(List_a)
+print("Smallest number in list is :",V)
