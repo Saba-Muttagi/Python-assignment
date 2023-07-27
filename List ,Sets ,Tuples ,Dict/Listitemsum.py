9183a1a608d62a5f321f7ce59cf1c002ebc31892
sabam <sabasam31@gmail.com>
2023-07-27 22:50:08 +0530

    Sum all the items in a list

---
+++ List ,Sets ,Tuples ,Dict/Listitemsum.py
@@ -0,0 +1,13 @@
+
+'''
+@Author: Saba Muttagi
+@Date: 2023-07-27
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-07-27
+@Title : Python program to sum all the items in a list.
+'''
+
+
+myList=[1,3,5,7,9,11,13,15,17,19]
+X=sum(myList)
+print("Sum of all items in a list is :",X)
