d0fa74161fda738110c2bc1c4489ef9c0d0f385d
sabam <sabasam31@gmail.com>
2023-07-30 14:26:42 +0530

    Appending one list to second list

---
+++ List ,Sets ,Tuples ,Dict/appendList.py
@@ -0,0 +1,14 @@
+
+'''
+@Author: Saba Muttagi
+@Date: 2023-07-29
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-07-29
+@Title : Python program to append a list to the second list.
+'''
+
+U=["apple","orange","grapes","mango"]
+V=["red","blue","green"]
+ 
+V.append(U)
+print("Append List :",V)
