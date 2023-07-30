dd76746953ec37f8ad65a9dd20a506b82f022645
sabam <sabasam31@gmail.com>
2023-07-30 14:29:50 +0530

    Clone or Copy a list

---
+++ List ,Sets ,Tuples ,Dict/clone_or_copylist.py
@@ -0,0 +1,18 @@
+
+'''
+@Author: Saba Muttagi
+@Date: 2023-07-29
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-07-29
+@Title : Python program to clone or copy a list.
+'''
+
+List2=["Lotus","Rose","Daisy","Lavendar","Orchid"]
+NewList=List2.copy()
+print(NewList)
+
+#Or
+
+List2=["Lotus","Rose","Daisy","Lavendar","Orchid"]
+NewList=list(List2)
+print(NewList)
