b7f6819783f8dea1b44d6d352a48e312d9f1eb54
sabam <sabasam31@gmail.com>
2023-07-31 17:13:36 +0530

    Converting list to tuple

---
+++ List ,Sets ,Tuples ,Dict/List_to_tuple.py
@@ -0,0 +1,12 @@
+
+'''
+@Author: Saba Muttagi
+@Date: 2023-07-31
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-07-31
+@Title : Python program to convert a list to a tuple.
+'''
+
+this_List=["apple","banana","cherry","grapes","mango"]
+this_Tuple=tuple(this_List)
+print(this_Tuple)
