e949036357c67b441e02fb6dbe8bb4853a7ab072
sabam <sabasam31@gmail.com>
2023-07-31 17:18:30 +0530

    creating tuple with different data types

---
+++ List ,Sets ,Tuples ,Dict/TupleWdDiffDataType.py
@@ -0,0 +1,11 @@
+
+'''
+@Author: Saba Muttagi
+@Date: 2023-07-30
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-07-30
+@Title : Python program to create a tuple with different data types
+'''
+
+Tuple_1=("Sea",12,False,"hello",89)
+print(Tuple_1)
