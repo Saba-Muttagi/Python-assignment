6d8e2bae3e52cf533ce22620b9a4640bba936f2b
sabam <sabasam31@gmail.com>
2023-07-31 17:10:47 +0530

    Creating a tuple

---
+++ List ,Sets ,Tuples ,Dict/Create_Tuple.py
@@ -0,0 +1,11 @@
+
+'''
+@Author: Saba Muttagi
+@Date: 2023-07-30
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-07-30
+@Title : Python program to create a tuple.
+'''
+
+A_tuple=("Rice","Wheat","Oats","Barley")
+print(A_tuple)
