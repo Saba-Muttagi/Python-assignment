336b766e243256ab9e16c4be5db95f43a724619c
sabam <sabasam31@gmail.com>
2023-07-31 17:08:16 +0530

    checking existence of item in tuple

---
+++ List ,Sets ,Tuples ,Dict/CheckElemntInTuple.py
@@ -0,0 +1,16 @@
+
+'''
+@Author: Saba Muttagi
+@Date: 2023-07-31
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-07-31
+@Title :Python program to check whether an element exists within a tuple
+'''
+
+Tuple_Y=("rose","daisy","lotus","lily")
+ 
+x=input("Enter element :")
+if x in Tuple_Y:
+  print("The element is present")
+else:
+  print("Not present")
