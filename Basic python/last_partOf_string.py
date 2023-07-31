90c175699af294a015a5d3a44699abfeebd0464d
sabam <sabasam31@gmail.com>
2023-07-31 22:05:05 +0530

    getting last part of string

---
+++ Basic python/last_partOf_string.py
@@ -0,0 +1,16 @@
+
+'''
+@Author: Saba Muttagi
+@Date: 2023-07-31
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-07-31
+@Title : Python program to get the last part of a string before a specified character.
+https://www.w3resource.com/python-exercises
+https://www.w3resource.com/python
+'''
+
+stringA="https://www.w3resource.com/python-exercises"
+stringB=stringA.split("-")
+#print(stringB)
+print("String after removing last part :")
+print(stringA)
