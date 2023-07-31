2aa2eccb55dccdd45eb6373c1c843ba09e105a16
sabam <sabasam31@gmail.com>
2023-07-31 17:20:53 +0530

    unpacking tuple into variables

---
+++ List ,Sets ,Tuples ,Dict/Unpack_Tuple.py
@@ -0,0 +1,17 @@
+
+'''
+@Author: Saba Muttagi
+@Date: 2023-07-30
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-07-30
+@Title : Python program to unpack a tuple in several variables.
+'''
+
+theTuple=("orange","grapes","mango","guava")
+(yellow,red,blue,grey)=theTuple
+
+print("Tuples into variables: ")
+print(yellow)
+print(red)
+print(blue)
+print(grey)
