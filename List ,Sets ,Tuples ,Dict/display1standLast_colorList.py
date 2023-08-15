1d8b0d90bf5e9d27c7c0ba81fda106e7ad557cd8
sabam <sabasam31@gmail.com>
2023-08-15 22:47:58 +0530

    program to display the first and last colors from the list

---
+++ List ,Sets ,Tuples ,Dict/display1standLast_colorList.py
@@ -0,0 +1,14 @@
+
+'''
+@Author: Saba Muttagi
+@Date: 2023-08-08
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-08-08
+@Title :Python program to display the first and last colors from the following list.
+color_list = ["Red","Green","White" ,"Black"]
+'''
+
+color_List=["Red","Green","White","Black"]
+subList=color_List[0::3]
+
+print(subList)
