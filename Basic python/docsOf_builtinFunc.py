8a0f21b04dcf2b1dbd46dd7f853934a6f8c6e1bd
sabam <sabasam31@gmail.com>
2023-08-15 22:37:52 +0530

    print the documents

---
+++ Basic python/docsOf_builtinFunc.py
@@ -0,0 +1,19 @@
+
+'''
+@Author: Saba Muttagi
+@Date: 2023-08-12
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-08-12
+@Title :Python program to print the documents (syntax, description etc.) of Python built-in
+function(s).
+Sample function : abs()
+Expected Result :
+abs(number) -> number
+Return the absolute value of the argument.
+'''
+
+
+print(abs.__doc__)
+
+u= -778.90
+print('Absolute value is :',abs(u))
