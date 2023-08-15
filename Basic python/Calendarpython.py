282c576c42359f62b8299ed974cf5db91d37aa67
sabam <sabasam31@gmail.com>
2023-08-15 22:10:07 +0530

    Print the calendar of a given month and year

---
+++ Basic python/Calendarpython.py
@@ -0,0 +1,16 @@
+
+'''
+@Author: Saba Muttagi
+@Date: 2023-08-12
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-08-12
+@Title : Python program to print the calendar of a given month and year.
+Note : Use 'calendar' module
+'''
+
+import calendar
+
+y=2023
+m=8
+
+print(calendar.month(y,m))
