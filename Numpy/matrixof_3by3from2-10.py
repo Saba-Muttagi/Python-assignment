fefd20db7dc2a87ca9b49ea1d7ed0d77f50c295b
sabam <sabasam31@gmail.com>
2023-08-17 14:25:44 +0530

    Creating a 3x3 matrix with values ranging from 2 to 10

---
+++ Numpy/matrixof_3by3from2-10.py
@@ -0,0 +1,17 @@
+
+'''
+@Author: Saba Muttagi
+@Date: 2023-08-17
+@Last Modified by: Saba Muttagi
+@Last Modified : 2023-08-17
+@Title :Create a 3x3 matrix with values ranging from 2 to 10.
+Expected Output:
+[[ 2 3 4]
+[ 5 6 7]
+[ 8 9 10]]
+'''
+
+import numpy as np
+
+arr=np.array([[2,3,4],[5,6,7],[8,9,10]])
+print(arr)
